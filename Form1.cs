c675e38fbdb77ec9b39b5871119569119276ee1a
LX.. <1659404657@qq.com>
2024-10-31 20:14:55 +0800

    设置窗口list和操作绑定

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel;
+
 namespace Expanding_Langton_s_And;
 
 public partial class Form1 : Form {
+    private BindingList<ObservableKeyValue> bindingList;
     public Form1() {
         InitializeComponent();
         startButton.Click += StartButtonOnClick;
         pauseButton.Click += PauseButtonOnClick;
         stopBotton.Click += StopBottonOnClick;
+
+        bindingList = new BindingList<ObservableKeyValue>(
+            LantongsAnt.operations.Select(kv => new ObservableKeyValue { Key = kv.Key, Value = kv.Value }).ToList());
+        stepsList.DataSource = bindingList;
+        stepsList.DisplayMember = "Key";  // 显示的内容
     }
 
     void StopBottonOnClick(object? sender, EventArgs e) {
@@ -21,5 +29,37 @@ public partial class Form1 : Form {
 
     void StartButtonOnClick(object? sender, EventArgs e) {
         Program.manualResetEvent.Set();
+    }
+
+    private void UpdateDictionary(SColor key, bool newValue)
+    {
+        if (LantongsAnt.operations.ContainsKey(key))
+        {
+            LantongsAnt.operations[key] = newValue;
+
+            var item = bindingList.FirstOrDefault(x => x.Key == key);
+            if (item != null)
+            {
+                item.Value = newValue; // 触发 INotifyPropertyChanged
+            }
+        }
+    }
+
+    private void AddToDictionary(SColor key, bool value)
+    {
+        LantongsAnt.operations[key] = value;
+        bindingList.Add(new ObservableKeyValue { Key = key, Value = value });
+    }
+
+    private void RemoveFromDictionary(SColor key)
+    {
+        if (LantongsAnt.operations.Remove(key))
+        {
+            var item = bindingList.FirstOrDefault(x => x.Key == key);
+            if (item != null)
+            {
+                bindingList.Remove(item);
+            }
+        }
     }
 }
---
+++ ObservableKeyValue.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel;
+
+namespace Expanding_Langton_s_And {
+    public class ObservableKeyValue : INotifyPropertyChanged
+    {
+        private SColor key;
+        private bool value;
+
+        public SColor Key
+        {
+            get => key;
+            set
+            {
+                key = value;
+                OnPropertyChanged(nameof(Key));
+            }
+        }
+
+        public bool Value
+        {
+            get => value;
+            set
+            {
+                value = value;
+                OnPropertyChanged(nameof(Value));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
